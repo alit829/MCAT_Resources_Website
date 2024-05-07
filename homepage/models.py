2f5f04f5fbfbf0b8bc6c72f9888c12d588255ba2
Ali Tavakoli <alitavakoli2011@yahoo.com>
2024-05-07 14:28:27 -0700

    minor change for reddit link buttons

--- homepage/models.py
+++ homepage/models.py
@@ -4,4 +4,4 @@ from django.db import models
 
 class Postlink(models.Model):
     mcat_redditpost_link = models.CharField(max_length=2083, blank=True)
-    dat_redditpost_link =  models.CharField(max_length=2083, blank=True)
+    dat_redditpost_link = models.CharField(max_length=2083, blank=True)
