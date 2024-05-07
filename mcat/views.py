b37b3e3b60126aba7e0700c2bee98b1f268464ce
Ali Tavakoli <alitavakoli2011@yahoo.com>
2024-05-07 12:55:22 -0700

    removed an extra forward slash causing an error as well as trying new changes such as using relative path rather than absolute path.

--- mcat/views.py
+++ mcat/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpRequest
-from dat.models import Resource # accidentally put down the mcat resources in the DAT resources tab in the admin webpage
+from dat.models import Resource  # accidentally put down the mcat resources in the DAT resources tab
+                                    # in the admin webpage
 
 # Create your views here.
+
+
 def index(request):
     resources = Resource.objects.all()
     return render(request, 'mcat.html', {'resources': resources})
